5981e00f8d30ff771aa90b8625bb4437f411d0ae
Collin Worth <worthcollin@gmail.com>
2024-04-07 11:29:40 -0700

    fixed sym structure

--- symtbl.h
+++ symtbl.h
@@ -4,10 +4,10 @@
 #define NSYMS (3)
 
 struct sym {
-  sym *next;
   char *name;
   double value;
-} sym_tbl[NSYMS];
+  struct sym *next;
+}; //sym_tbl[NSYMS];
 
 int sym_count(void);
 int list_count(void);
